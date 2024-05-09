96b4431ea9fb906349c60a8a6348616f750420b6
santilaguado <santiagolaguado57@gmail.com>
2024-05-08 20:54:08 -0500

    Dia 6 - Normalizacion

---
+++ Dia 6/normalizacion.sql
@@ -0,0 +1,15 @@
+
+
+
+--Que es normalizar? Reducir las posibles redundancias de datos y
+--con ello mejorar la integridad de los datos
+
+--Pasos principales para la normalizacion
+--Primera Forma Normal (1nf) "Reducir"
+--Segunda Forma Normal (2nf) "Redundancia"
+--Tercera Forma Normal (3nf) "Independencia"
+
+--En la 1NF se busca eliminar las repeticiones de datos y asegurar con ello
+-- que cada columna contenga un solo valor atomico
+-- VALOR ATOMICO: Dato que no se puede dividir, sea por convivencia o por
+-- especificacion del lenguaje
